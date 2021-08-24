34a76265cc5a996c0444808ba5e5021cd59d15ac
Madison <madivallarino@gmail.com>
2021-08-24 11:01:29 -0500

    kinda works now

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-fetch('https://www.theaudiodb.com/api/v1/json/1/search.php?s=daft_punk')
+
+document.querySelector("#create-task-form").addEventListener('submit', (e) =>  {
+  e.preventDefault();
+  userInput()
+})
+///newInput to be a global variable to allow fetch to use the information
+let newInput = [];
+
+function userInput()
+{ let a = document.querySelector("#new-zip-input-box").value //querySelector //bootcamp will need to be changed to querySelector the user input value
+let newLetter = [];
+for (let i = 0; i < a.length; i++) {
+  if (a[i] === ' '){
+    newLetter += '_';
+    newInput += newLetter;
+  } else {
+    newInput += a[i];
+  }}
+ return newInput
+}
+fetch(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${newInput}`)
 .then(resp => resp.json())
 .then(data => {postAlbum(data)}) //returns an object 
 
@@ -7,23 +27,7 @@ function postAlbum(object){
 }
 
 
-function userInput()
-{
-  let a = "boot camp"
-let text = []; 
-let newLetter = [];
-let newWord = [];
 
-
-for (let i = 0; i < a.length; i++) {
-  if (a[i] === ' '){
-    newLetter += '_';
-    newWord += newLetter;
-  } else {
-    newWord += a[i];
-  }
-}
-console.log(newWord);}
 
 
 
