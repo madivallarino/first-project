f235ceea4cf476ea2e892321ee98e4729086ba7b
Madison <madivallarino@gmail.com>
2021-08-24 15:56:21 -0500

    as close as ive gotten

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-
-document.querySelector("#create-task-form").addEventListener('submit', (e) =>  {
+document.querySelector("#create-task-form").addEventListener('submit', (e) => {
   e.preventDefault();
-  userInput()
+  let a = document.querySelector("#new-zip-input-box").value
+  forLoop(a)
+  console.log(a)
+  // fetchInformation(newWord)
 })
-///newInput to be a global variable to allow fetch to use the information
-let newInput = [];
+ 
+function forLoop(word){
+    let newWord = "";
+  let newLetter = [];
+  let a = word
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === ' '){
+        newLetter += '_';
+        newWord += newLetter;
+      } else {
+        newWord += a[i];
+      } }
+      return newWord 
+    }
 
-function userInput()
-{ let a = document.querySelector("#new-zip-input-box").value //querySelector //bootcamp will need to be changed to querySelector the user input value
-let newLetter = [];
-for (let i = 0; i < a.length; i++) {
-  if (a[i] === ' '){
-    newLetter += '_';
-    newInput += newLetter;
-  } else {
-    newInput += a[i];
-  }}
- return newInput
-}
-fetch(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${newInput}`)
-.then(resp => resp.json())
-.then(data => {postAlbum(data)}) //returns an object 
-
-function postAlbum(object){
-    console.log(object)
-}
-
-
-
-
-
-
-
-
+    function fetchInformation(newWord){
+    fetch(`https://www.theaudiodb.com/api/v1/json/1/search.php?s=${newWord}`)
+   .then(resp => resp.json())
+   .then(data => console.log(data)) 
+    }
